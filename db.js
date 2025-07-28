6294bbe816d06a5164c6cdce3849c5ab42c0acd6
MOHID HAFEEZ <123815787+mohidhafeez@users.noreply.github.com>
2025-07-28 16:53:04 +0500

    adding menu

---
+++ db.js
@@ -0,0 +1,46 @@
+// TO MAKE CONNECTION BETWEEN MONGO_DB AND THE NODE.JS
+
+const mongoose = require("mongoose");
+
+//DEFINE THE MONGO DB CONNECTION URL
+
+const mongoURL = "mongodb://localhost:27017/hotels"; // REPLEACE WITH YOUR DATABASE NAME
+
+//SET UP CONNECTION
+mongoose.connect(mongoURL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// FOR INTERACTION
+
+const db = mongoose.connection;
+
+//EVENT LISTNER FOR CONNECTION
+
+db.on("connected", () => {
+  console.log(`
+    ---------------------------
+    Connected to MONGODB server 
+    ---------------------------
+    `);
+});
+db.on("error", (err) => {
+  console.log(`
+    --------------------------------
+    MONGODB connection error :${err}
+    --------------------------------
+    
+    `);
+});
+db.on("disconnected", () => {
+  console.log(`
+    --------------------
+    MONGODB disconnected
+    --------------------
+    `);
+});
+
+//EXPORT TO DATABASE CONNECTION
+
+module.exports = db;
