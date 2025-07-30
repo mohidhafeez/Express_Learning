71d265b79fcaa2555cd300c1d9ff986b73475e7d
MOHID HAFEEZ <123815787+mohidhafeez@users.noreply.github.com>
2025-07-30 11:44:37 +0500

    person delete done

--- routes/personsRoute.js
+++ routes/personsRoute.js
@@ -74,4 +74,21 @@ personRouter.put("/:id", async (req, res) => {
   }
 });
 
+personRouter.delete("/:id", async (req, res) => {
+  try {
+    const personId = req.params.id;
+
+    const response = await Person.findByIdAndDelete(personId);
+
+    if (!response) {
+      res.status(404).json({ error: "Person not found" });
+    }
+    console.log("delete successfully");
+    res.status(200).json(response);
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({ error: "internal server error" });
+  }
+});
+
 module.exports = personRouter;
